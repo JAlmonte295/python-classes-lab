1a9cb1f9ac176af1cc4dd2bf6521b7690888c1ad
jalmontegit <jalmonte721@gmail.com>
2025-12-03 20:30:25 -0500

    Level up for the Lab Completed

--- exercises.py
+++ exercises.py
@@ -2,14 +2,19 @@
 
 class Game():
   def __init__(self):
+    self.x_score = 0
+    self.o_score = 0
+    self.reset()
+
+  def reset(self):
     self.turn = "X"
     self.tie = False
     self.winner = None
     self.board = {
       'a1': None, 'b1': None, 'c1': None,
       'a2': None, 'b2': None, 'c2': None,
-      'a3': None, 'b3': None, 'c3': None
-    }
+      'a3': None, 'b3': None, 'c3': None,
+    }    
 
   def print_board(self):
     b = self.board
@@ -61,19 +66,39 @@ class Game():
   
   def switch_turn(self):
     self.turn = "O" if self.turn == "X" else "X"
-  
+
+  def play_again(self):
+    while True:
+      choice = input("Do you want to play again? (y/n) ").lower()
+      if choice == "y":
+        return True
+      elif choice == "n":
+        return False
+      else:
+        print("Invalid choice. Try again.")
+
   def play_game(self):
     print("Welcome to Tic-Tac-Toe!")
     while self.winner is None and not self.tie:
       self.render()
       self.get_move()
-      if not self.check_winner():
-        self.switch_turn()
       self.check_winner()
       self.check_tie()
+      if self.winner is None and not self.tie:
+        self.switch_turn()
     self.print_board()
+    if self.winner == 'X':
+      self.x_score += 1
+    elif self.winner == 'O':
+      self.o_score += 1
     self.print_message()
+    print(f"Score: Player X - {self.x_score} | Player O - {self.o_score}")
     
 game_instance = Game()
 
-game_instance.play_game()
+while True:
+  game_instance.play_game()
+  if not game_instance.play_again():
+    print("Thanks for playing!")
+    break
+  game_instance.reset()
